0213e86cb455eb95805fbe7ce2c3ffe47169343c
j0nimost <john.nyingi@live.com>
2018-12-21 22:35:26 +0300

    ft-162757626 added User View

---
+++ API/app/api/v1/views/UserView.py
@@ -0,0 +1,44 @@
+from flask import request, jsonify, abort, make_response
+from ..models.UserModel import users,User
+from ..utils.validator import password_match
+from . import auth
+
+@auth.route('/register', methods=['POST'])
+def register():
+    '''register a user to the platform'''
+    if request.get_json():
+        data = request.get_json()
+        username = data['username']
+        email = data['email']
+        password = data['password']
+        confirm_password = data['confirm_password']
+        
+        if password_match(password, confirm_password):
+            '''Add user to the data structure'''
+            user = User(email,username,password)
+            user.add_user()
+            return make_response(jsonify({"message":"Successfully Registered"}),201)
+        else:
+            abort(400, 'Password and Confirm Password don\'t match')
+    else:
+        abort(400,"application/json expected")
+    
+
+@auth.route('/login', methods=['POST'])
+def login():
+    '''login a user to the platform'''
+    if request.get_json():
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        user = User()
+        find_usr = user.find_user(username)
+        if find_usr:
+            '''Check password match'''
+            if user.check_password(find_usr['password'],password):
+                return make_response(jsonify({"message":"Successfully Logged In"}),200)
+            else:
+                abort(400,"Invalid Password")
+        else:
+            return make_response(jsonify({"message":"User not Found"},404))
