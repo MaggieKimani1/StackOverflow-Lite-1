8288c103861e5a4947dadf065752664666743352
j0nimost <john.nyingi@live.com>
2018-12-22 19:36:15 +0300

    ch-162569809 added env checks and registered blueprints

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
+#import sys
+#sys.path.append('/home/j0nimost/Documents/Andela/StackOverflow-lite/API/')
+from .api.v1.views.UserView import auth
 from instance.config import app_config
-"""Importing configurations from config file"""
+
+"""This is the main app set up"""
 
 def create_app(config_name):
     '''Creating app, setting up configurations to run the app'''
     app = Flask(__name__,instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    
+    if not config_name:
+        app.config.from_object(app_config['development'])
+    else:
+        app.config.from_object(app_config[config_name])
+
+    app.config.from_pyfile('config.py',silent=True)
+
+    app.register_blueprint(auth)
+
     return app
