55ecd255c0b10dd2dd474a0c048d90530b575838
j0nimost <john.nyingi@live.com>
2018-12-23 10:55:38 +0300

    ft-162757626 added password checkers and generators to validator

--- API/app/api/v1/utils/validator.py
+++ API/app/api/v1/utils/validator.py
@@ -1,3 +1,12 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 def password_match(password, confirmpass):
     if password == confirmpass:
         return True
+
+def set_password(password):
+    password_hash = generate_password_hash(password)
+    return password_hash
+            
+def check_password(passwd_hash, password):
+    return check_password_hash(passwd_hash,password)
